5a7046458c28adddc4c4f1409446061e955b37d1
BrotherlyHamlet16 <80749000+Clever-Niwagaba@users.noreply.github.com>
2025-05-05 09:31:35 +0300

    Tailwind configured

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,23 +6,30 @@
           content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
-    @vite(['resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-    <div>
-        <nav>
+<body class="bg-black text-white">
+    <div class="px-10">
+        <nav class='border-b border-white/10 flex justify-between items-center bg-red-500 py-4'>
             <div>
-                <a href="">
+                <a href="/">
                     <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="Logo" width="100" height="100">
                 </a>
             </div>
 
-            <div>Links</div>
+            <div class="space-x-6 font-bold">
+                <a href="">Jobs</a>
+                <a href="">Careers</a>
+                <a href="">Salaries</a>
+                <a href="">Companies</a>
+            </div>
 
-            <div>Post a Job</div>
+            <div>
+                <a href="">Post a Job</a>
+            </div>
         </nav>
 
-        <main>
+        <main class="mt-10 max-w-[986px]">
             {{$slot}}
         </main>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,26 @@
 <x-layout>
-    Hello World!
+    <section>
+        <h1>Top Jobs</h1>
+
+        <div>
+            <div>
+                <div>BNMG</div>
+                
+                <div>
+                    <h1>Video Producer</h1>
+                    <p>Full Time - From $60,000</p>
+                </div>
+
+                <div>
+                    <div>
+                        <a href=""></a>
+                        <a href=""></a>
+                        <a href=""></a>
+                    </div>
+
+                    <img src="http://placehold.it/42/42" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
 </x-layout>
