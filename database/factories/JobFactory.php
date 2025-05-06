9dd9781ea634c53cc59e5ab63a10b19dfa56e9d3
BrotherlyHamlet16 <80749000+Clever-Niwagaba@users.noreply.github.com>
2025-05-06 13:39:11 +0300

    architecture start

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->jobTitle,
+            'salary' => fake()->randomElement(['500,000k', '1,500,000', '3,500,000']),
+            'location' => 'Remote',
+            'schedule' => 'Full Time',
+            'url' => fake()->url,
+            'featured' => false
+        ];
+    }
+}
---
+++ tests/Unit/JobTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Models\Employer;
+use App\Models\Job;
+
+class JobTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function belongsToAnEmployer(): void
+    {
+        // $this->assertTrue(true);
+
+        //Arrange
+        $employer = Employer::factory()->create();
+        $job = Job::factory()->create([
+            'employer_id' => $employer->id,
+        ]);
+
+        //Act and Assert
+        // expect($job->employer->is($employer))->toBeTrue();
+    }
+}
+
+it('can have tags', function () {
+    // AAA
+    $job = Job::factory()->create();
+
+    $job->tag('Frontend');
+
+    expect($job->tags)->toHaveCount(1);
+});
