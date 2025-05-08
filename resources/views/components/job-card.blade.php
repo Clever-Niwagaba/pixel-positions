1381c0f37711591b7e0313917467a9672339ee5a
BrotherlyHamlet16 <80749000+Clever-Niwagaba@users.noreply.github.com>
2025-05-08 10:35:55 +0300

    Loading info from seeded database

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,16 +1,16 @@
 @props(['job'])
 <x-panel class="flex flex-col text-center">
-    <div class="self-start text-sm">BNMG</div>
+    <div class="self-start text-sm">{{$job->employer->name}}</div>
                 
         <div class="py-8">
-            <h1 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-400">Video Producer</h1>
-            <p class="text-sm mt-4">Full Time - From $60,000</p>
+            <h1 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-400">{{$job->title}}</h1>
+            <p class="text-sm mt-4">{{$job->salary}}</p>
         </div>
 
         <div class="flex justify-between items-center mt-auto">
             <div>
                 @foreach ($job->tags as $tag)
-                    <x-tag :$tag size="small">Backend</x-tag>
+                    <x-tag :$tag size="small"/>
                 @endforeach
                 
             </div>
